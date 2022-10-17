221be25b6b4556dac39f1c4b82dad1796fb66cfd
Paolo Velati <paolo.velati@gmail.com>
2022-10-17 09:06:07 +0000

    Fix typo in script, removed all previous releases

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-echo "------------- VARIABLES ----------------"
+# ------------- DEFINE VARIABLES ----------------"
 echo $BUILD_ARCH | tr 'a-z' 'A-Z' > build_arch_upper_tmp
 export BAUP=$(cat build_arch_upper_tmp)
 export BUILD_NUMBER=$(curl -s http://download.proxmox.com/debian/pve/dists/bullseye/pve-no-subscription/binary-amd64/Packages | grep ^Filename  | grep pve-kernel-5 | grep amd64.deb$ | sort -V | grep -oP '5.15.\d+-\d+' | tail -1 | grep -o .$)
 
-echo "------------- CLONE REPO ----------------"
+# ------------- CLONE SOURCE REPO ----------------"
 git clone --depth 1 --branch pve-kernel-5.15 https://git.proxmox.com/git/pve-kernel.git
 
 
-echo "------------- ENTER FOLDER ----------------"
-echo "cd pve-kernel"
+# ------------- ENTER FOLDER ----------------"
 cd pve-kernel
 
 
-echo "------------- ADD PATCH ----------------"
+echo "------------- ADD CPU PATCH ----------------"
 wget -O patches/kernel/0032-add-uarch.patch https://raw.githubusercontent.com/graysky2/kernel_compiler_patch/master/more-uarches-for-kernel-5.15-5.16.patch
 
 
-echo "------------- CONFIGURE ----------------"
-echo "sed -i \"s/^.*\bKREL=\b.*$/KREL=$BUILD_NUMBER/g\" Makefile"
+# ------------- CHANGE CONFIGURATION ----------------"
 sed -i "s/^.*\bKREL=\b.*$/KREL=$BUILD_NUMBER/g" Makefile
-echo "sed -i \"s/\EXTRAVERSION=-\${KREL}-pve/EXTRAVERSION=-\${KREL}-pve-$BUILD_ARCH/g\" Makefile"
-echo "sed -i 's/\-e CONFIG_PAGE_TABLE_ISOLATION/\-e CONFIG_PAGE_TABLE_ISOLATION \\/g' debian/rules"
+sed -i "s/\EXTRAVERSION=-\${KREL}-pve/EXTRAVERSION=-\${KREL}-pve-$BUILD_ARCH/g" Makefile
 sed -i 's/\-e CONFIG_PAGE_TABLE_ISOLATION/\-e CONFIG_PAGE_TABLE_ISOLATION \\/g' debian/rules
-echo "sed -i '/^\-e CONFIG_PAGE_TABLE_ISOLATION.*/a \-d CONFIG_GENERIC_CPU \\\n\-e CONFIG_MARCHITECTURE \\\n\-e CONFIG_X86_INTEL_USERCOPY \\\n\-e CONFIG_X86_USE_PPRO_CHECKSUM \\\n\-e CONFIG_X86_P6_NOP \\\n\-e CONFIG_CC_HAS_RETURN_THUNK \\\n\-e CONFIG_SPECULATION_MITIGATIONS \\\n\-e CONFIG_RETHUNK \\\n\-e CONFIG_CPU_UNRET_ENTRY \\\n\-e CONFIG_CPU_IBPB_ENTRY \\\n\-e CONFIG_CPU_IBRS_ENTRY' debian/rules"
 sed -i '/^\-e CONFIG_PAGE_TABLE_ISOLATION.*/a \-d CONFIG_GENERIC_CPU \\\n\-e CONFIG_MARCHITECTURE \\\n\-e CONFIG_X86_INTEL_USERCOPY \\\n\-e CONFIG_X86_USE_PPRO_CHECKSUM \\\n\-e CONFIG_X86_P6_NOP \\\n\-e CONFIG_CC_HAS_RETURN_THUNK \\\n\-e CONFIG_SPECULATION_MITIGATIONS \\\n\-e CONFIG_RETHUNK \\\n\-e CONFIG_CPU_UNRET_ENTRY \\\n\-e CONFIG_CPU_IBPB_ENTRY \\\n\-e CONFIG_CPU_IBRS_ENTRY' debian/rules
-echo "sed -i \"s/CONFIG_MARCHITECTURE/CONFIG_M$BAUP/g\" debian/rules"
 sed -i "s/CONFIG_MARCHITECTURE/CONFIG_M$BAUP/g" debian/rules
 
 
-echo "------------- START BUILD ----------------"
+# ------------- START BUILD ----------------"
 echo "yes \"\" | make PVE_BUILD_CFLAGS=\"-march=$BUILD_ARCH\" deb"
 yes "" | make PVE_BUILD_CFLAGS="-march=$BUILD_ARCH" deb
+
+
+#echo "------------- RENAME DEBS ----------------"
+# Test if needed for fixing reprepro issue
+#for f in pve-*.deb; do mv "$f" "$(echo "$f" | sed s/\.deb/_$BUILD_ARCH\.deb/)"; done
