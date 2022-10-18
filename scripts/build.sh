4ad7c2baef004a89342c05743fab9cff61815ef9
Paolo Velati <paolo.velati@gmail.com>
2022-10-18 11:46:22 +0000

    Fix comments in script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# ------------- DEFINE VARIABLES ----------------"
+# ------------- DEFINE VARIABLES ----------------
 echo $BUILD_ARCH | tr 'a-z' 'A-Z' > build_arch_upper_tmp
 export BAUP=$(cat build_arch_upper_tmp)
 export BUILD_NUMBER=$(curl -s http://download.proxmox.com/debian/pve/dists/bullseye/pve-no-subscription/binary-amd64/Packages | grep ^Filename  | grep pve-kernel-5 | grep amd64.deb$ | sort -V | grep -oP 'kernel-5.15.\d+-\d+' | tail -1 | grep -o .$)
 export PACKAGE_NUMBER=$(curl -s http://download.proxmox.com/debian/pve/dists/bullseye/pve-no-subscription/binary-amd64/Packages | grep ^Filename  | grep pve-kernel-5 | grep amd64.deb$ | sort -V | grep -oP 'pve_5.15.\d+-\d+' | tail -1 | grep -o .$)
 
-# ------------- CLONE SOURCE REPO ----------------"
+# ------------- CLONE SOURCE REPO ----------------
 git clone --depth 1 --branch pve-kernel-5.15 https://git.proxmox.com/git/pve-kernel.git
 
 
-# ------------- ENTER FOLDER ----------------"
+# ------------- ENTER FOLDER ----------------
 cd pve-kernel
 
 
-echo "------------- ADD CPU PATCH ----------------"
+# ------------- ADD CPU PATCH ----------------
 wget -O patches/kernel/0032-add-uarch.patch https://raw.githubusercontent.com/graysky2/kernel_compiler_patch/master/more-uarches-for-kernel-5.15-5.16.patch
 
 
-# ------------- CHANGE CONFIGURATION ----------------"
+# ------------- CHANGE CONFIGURATION ----------------
 sed -i "s/^.*\bKREL=\b.*$/KREL=$BUILD_NUMBER/g" Makefile
 sed -i "s/^.*\bPKGREL=\b.*$/PKGREL=$PACKAGE_NUMBER/g" Makefile
 sed -i "s/\EXTRAVERSION=-\${KREL}-pve/EXTRAVERSION=-\${KREL}-pve-$BUILD_ARCH/g" Makefile
@@ -27,5 +27,5 @@ sed -i '/^\-e CONFIG_PAGE_TABLE_ISOLATION.*/a \-d CONFIG_GENERIC_CPU \\\n\-e CON
 sed -i "s/CONFIG_MARCHITECTURE/CONFIG_M$BAUP/g" debian/rules
 
 
-# ------------- START BUILD ----------------"
+# ------------- START BUILD ----------------
 yes "" | make PVE_BUILD_CFLAGS="-march=$BUILD_ARCH" deb
